4430b81925f3cd0e20606553c312a5ad764f1806
Eidan Khan <76518500+eidankhan@users.noreply.github.com>
2025-07-16 17:57:30 +0100

    Building the Movie Catalog Service API

---
+++ movie-catalog-service/src/main/java/io/javabrains/movie_catalog_service/MovieCatalogResource.java
@@ -0,0 +1,21 @@
+package io.javabrains.movie_catalog_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        return Collections.singletonList(
+                new CatalogItem("Devdas","Romance", 3)
+        );
+    }
+}
